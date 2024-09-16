e806bb22d3d7323ca4884ecd82748fab2bba4330
Aman Pandey <pandeyaman636@gmail.com>
2024-09-17 00:46:03 +0530

    find age

--- function.js
+++ function.js
@@ -1,50 +1,71 @@
 
-function concantename(firstname, lastName){     // function parameter
-    var fullName = firstname + " "+ lastName;
-    return fullName;
-}
-let n = concantename(1,2)
-console.log(n);
+// function concantename(firstname, lastName){     // function parameter
+//     var fullName = firstname + " "+ lastName;
+//     return fullName;
+// }
+// let n = concantename(1,2)
+// console.log(n);
 
 
 
-function arithmetic(num1, num2){
-    let sum = num1 + num2
-    return sum
-}
-let total = arithmetic(2,3)
-console.log(total);
+// function arithmetic(num1, num2){
+//     let sum = num1 + num2
+//     return sum
+// }
+// let total = arithmetic(2,3)
+// console.log(total);
 
 
-function EvenOddCheck(number){
-    if(number%2==0)
-        return "Even"
-    else
-        return "Odd"      
+// function EvenOddCheck(number){
+//     if(number%2==0)
+//         return "Even"
+//     else
+//         return "Odd"      
 
-}
-let check = EvenOddCheck(4);
-console.log(check);
+// }
+// let check = EvenOddCheck(4);
+// console.log(check);
 
 
-function factorial(Num2){
-    if (Num2 === 0 || Num2 === 1) {
-        return 1;
-    } else {
-        return Num2 * factorial(Num2 - 1);
-    }
-}
+// function factorial(Num2){
+//     if (Num2 === 0 || Num2 === 1) {
+//         return 1;
+//     } else {
+//         return Num2 * factorial(Num2 - 1);
+//     }
+// }
 
-console.log(factorial(5));
+// console.log(factorial(5));
 
-function sumArray(Number3){
-    let sum = 0;
-    for (let i = 0; i < Number3.length; i++) {
-      sum+=Number3[i]
+// function sumArray(Number3){
+//     let sum = 0;
+//     for (let i = 0; i < Number3.length; i++) {
+//       sum+=Number3[i]
         
+//     }
+//     return sum;
+// }
+
+// console.log(sumArray([1,2,3,4,5,6]));
+
+
+
+function findAge(age, passName){
+    if (age>=18) {
+        return passName.concat(' is adult');
     }
-    return sum;
+    else{
+        if(age<18 && age>13) {
+            return passName.concat(' is teenager');
+
+        }else{
+            return passName.concat(' is child');
+
+        }
+    }
+    
+
 }
 
-console.log(sumArray([1,2,3,4,5,6]));
+console.log(findAge(13, "Aman"));
 
